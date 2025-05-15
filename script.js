68a9436395ae482bdfaf269033c93c13fe9d290d
aumshub <aumpatel2930@gmail.com>
2025-05-15 02:27:02 -0400

    some changes

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("JS Running...");
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', function(){
+        hamburger.classList.toggle('active')
+        navLinks.classList.toggle('active')
+    });
+    
+    
+    document.querySelectorAll('.nav-links a').forEach(link => {
+        link.addEventListener('click', function(){
+            hamburger.classList.remove('active')
+            navLinks.classList.remove('active')
+        })
+    })
+    
+    
+
+
+})
