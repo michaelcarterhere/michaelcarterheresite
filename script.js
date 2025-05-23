1dd93f3160172dfc45339a2305d5abf38b8e3b2f
aumshub <aumpatel2930@gmail.com>
2025-05-22 21:15:52 -0400

    some changes

--- script.js
+++ script.js
@@ -24,7 +24,40 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 
+
+    const contactForm = document.getElementById("contact-form")
+ contactMessage = document.getElementById("contact-message")
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_agjzg9g','template_ozrhde7','#contact-form','KG8w3aez8PwBvFfFc')
+    .then(() => {
+        //Show sent message
+        contactMessage.textContent = "Thank you! Your message has been sent successfully. I’ll be in touch soon—excited to connect and explore new ideas together! Sent "
+
+        // Remove message after five seconds
+        setTimeout (() =>{
+            contactMessage.textContent = ""
+        }, 5000)
+
+        //clear input field
+        contactForm.reset()
+    }), () =>{
+        //Show error message
+        contactMessage.textContent="Message not sent (service error) ❌"
+    }
+} 
+
+contactForm.addEventListener('submit', sendEmail)
     
+
+
+
+
+
+
 
 
 
